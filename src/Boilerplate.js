4e7dbc65bb91c25013d5ae158f6ad0fc11f8b31c
Hernán Felipe Serna Gómez <hfserna@gmail.com>
2021-09-07 21:47:02 -0500

    [FEAT] Crear minimal react boilerplate

---
+++ src/Boilerplate.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Boilerplate = () => {
+  return (
+      <h3>Boilerplate - ¡Hello world!</h3>
+  );
+};
+
+export default Boilerplate;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import Boilerplate from './Boilerplate';
+
+render(<Boilerplate />, document.getElementById('root'));
